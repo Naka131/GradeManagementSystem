a337db96b55e7150ce5b3b7c28493db8ffea6d91
k_hanaki <k_hanaki@LAPTOP-54MIMU5V>
2025-06-02 11:55:39 +0900

    修正

--- src/student/StudentGetClassAction.java
+++ src/student/StudentGetClassAction.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.Class;
 import dao.ClassNumberDAO;
@@ -14,7 +13,6 @@ public class StudentGetClassAction extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		HttpSession session = request.getSession(false);
 
 		String school_code = request.getParameter("school_code");
 
@@ -26,7 +24,6 @@ public class StudentGetClassAction extends Action {
 	    response.setContentType("application/json; charset=UTF-8");
 	    PrintWriter out = response.getWriter();
 
-	    // JSON配列として出力
 	    out.print("[");
 	    for (int i = 0; i < classList.size(); i++) {
 	        Class cla = classList.get(i);
@@ -35,7 +32,7 @@ public class StudentGetClassAction extends Action {
 	    }
 	    out.print("]");
 	    out.close();
-	    
+
 	    return null;
 
 	}
