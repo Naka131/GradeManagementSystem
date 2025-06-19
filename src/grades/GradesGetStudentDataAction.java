6db279ca5ee550d8e592763b458f66b8bed602e1
k-h79 <khanaki.ohara@gmail.com>
2025-06-19 11:45:50 +0900

    成績登録自動補完の修正

--- src/grades/GradesGetStudentDataAction.java
+++ src/grades/GradesGetStudentDataAction.java
@@ -13,6 +13,8 @@ public class GradesGetStudentDataAction extends Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter out = response.getWriter();
 
 		String student_id = request.getParameter("student_id");
 
@@ -21,21 +23,23 @@ public class GradesGetStudentDataAction extends Action {
 
 		List<Students> studentList = stDAO.searchdata(student_id);
 
-
-
-
-	    response.setContentType("application/json; charset=UTF-8");
-	    PrintWriter out = response.getWriter();
-
-	    out.print("[");
-	    for (int i = 0; i < studentList.size(); i++) {
-	        Students st = studentList.get(i);
-	        out.println("{\"school_code\":\"" + st.getSchool_code() + "\",\"class_number\":\"" + st.getClass_number() + "\",\"student_name\":\"" + st.getStudent_name() + "\"}");
-	        if (i < studentList.size() - 1) out.print(",");
-	    }
-	    out.print("]");
-	    out.close();
-
+		if (studentList.isEmpty()) {
+			out.print("[");
+				out.println("{\"school_code\":\"\",\"class_number\":\"\",\"student_name\":\"\"}");
+			out.print("]");
+			out.close();
+		} else {
+		
+		
+		    out.print("[");
+		    for (int i = 0; i < studentList.size(); i++) {
+		        Students st = studentList.get(i);
+		        out.println("{\"school_code\":\"" + st.getSchool_code() + "\",\"class_number\":\"" + st.getClass_number() + "\",\"student_name\":\"" + st.getStudent_name() + "\"}");
+		        if (i < studentList.size() - 1) out.print(",");
+		    }
+		    out.print("]");
+		    out.close();
+		}
 	    return null;
 
 	}
